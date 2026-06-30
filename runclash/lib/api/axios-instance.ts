13a51cd2a53f197fda9e8ad96cbcbc6a3c69d4d2
Shray <shilpshrayash90@gmail.com>
2026-06-30 19:01:01 +0545

    refactor(api): update axios instance

--- runclash/lib/api/axios-instance.ts
+++ runclash/lib/api/axios-instance.ts
@@ -1,13 +1,36 @@
 import axios from "axios";
+import { getTokenCookie as getTokenCookieClient } from "../cookies-client";
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL 
-    || "http://localhost:8089";
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8089";
 
 const axiosInstance = axios.create({
-    baseURL: BASE_URL,
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use(async (config) => {
+  let token: string | null = null;
+
+  if (typeof window === "undefined") {
+    const { getTokenCookie } = await import("../cookies.server");
+    token = await getTokenCookie();
+  } else {
+    token = getTokenCookieClient();
+  }
+
+  if (token) {
+    // Assign Authorization header without breaking Axios header types
+    if (!config.headers) {
+      config.headers = { Authorization: `Bearer ${token}` } as any;
+    } else {
+      (config.headers as any).Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default axiosInstance;
