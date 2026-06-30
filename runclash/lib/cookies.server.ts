c89b04738f59d7afd38bd0658592e567b411c959
Shray <shilpshrayash90@gmail.com>
2026-06-30 19:02:02 +0545

    feat(auth): add server cookie utilities

---
+++ runclash/lib/cookies.server.ts
@@ -0,0 +1,37 @@
+"use server";
+import { cookies } from "next/headers";
+
+export async function setTokenCookie(token: string) {
+  const cookieStore = await cookies();
+  cookieStore.set({
+    name: "auth_token",
+    value: token,
+    path: "/",
+  });
+}
+
+export async function getTokenCookie() {
+  const cookieStore = await cookies();
+  return cookieStore.get("auth_token")?.value || null;
+}
+
+export async function storeUserData(userData: any) {
+  const cookieStore = await cookies();
+  cookieStore.set({
+    name: "user_data",
+    value: JSON.stringify(userData),
+    path: "/",
+  });
+}
+
+export async function getUserData() {
+  const cookieStore = await cookies();
+  const userDataCookie = cookieStore.get("user_data")?.value;
+  return userDataCookie ? JSON.parse(userDataCookie) : null;
+}
+
+export async function clearAuthCookies() {
+  const cookieStore = await cookies();
+  cookieStore.delete("auth_token");
+  cookieStore.delete("user_data");
+}
